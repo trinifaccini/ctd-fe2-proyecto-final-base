e913f21eb8c046283689d3da7e6d92ce550dcc2b
Trinidad Faccini <trinifaccini@MacBook-Air-de-Trinidad.local>
2022-07-04 22:30:59 -0300

    Correcciones generales

--- src/features/bio/Bio.tsx
+++ src/features/bio/Bio.tsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
 import { NombresSimpsons, INFO_SIMPSONS } from "./constants";
 import styles from "./styles.module.css";
-
-import { StyledContainer, StyledButtonContainer, StyledBioImg, StyledBioName, StyledBioDesc, StyledButton } from "./styles";
+import { 
+  StyledContainer, 
+  StyledButtonContainer, 
+  StyledBioImg, 
+  StyledBioName, 
+  StyledBioDesc, 
+  StyledButton 
+} from "./styles";
 
 const Bio = () => {
-  const [bioActiva, setBioActiva] = useState(
-    INFO_SIMPSONS[NombresSimpsons.BART]
-  );
 
-  const onClick: (nombre: NombresSimpsons) => void = (nombre) =>
+  // Como default vemos a Bart
+  // INFO_SIMPSONS es un objeto con los diferentes personajes (objetos) 
+  const [bioActiva, setBioActiva] = useState(INFO_SIMPSONS[NombresSimpsons.BART]);
+
+  const onClick: (nombre: NombresSimpsons) => void = (nombre) => {
     setBioActiva(INFO_SIMPSONS[nombre]);
+  }
 
+  // Devuelve un botón con el nombre del personaje (creador de botones)
   const crearBotones = () => {
     return Object.keys(INFO_SIMPSONS).map((nombre: string) => (
       <StyledButton
         key={nombre as string}
         onClick={() => onClick(nombre as NombresSimpsons)}
-        active = {bioActiva.id === nombre ? true: false}
+        active = {bioActiva.id === nombre ? true : false}
       >
         {nombre}
       </StyledButton>
